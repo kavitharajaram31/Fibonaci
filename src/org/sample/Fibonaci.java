d0333a5929ec0af926b0cc4ed546e668ecad8694
kavitharajaram31 <kavitharajaram1993@gmail.com>
2019-05-16 11:39:26 +0530

    new git

---
+++ src/org/sample/Fibonaci.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+//import java.util.Scanner;
+
+public class Fibonaci {
+public static void main(String[] args) {
+int n = 5;
+int a =0;
+int b=1;
+System.out.println("fibo series ");
+for(int i=1;i<=5;i++) {
+	System.out.println(a+" ");
+int sum=a+b;
+a=b;
+b=sum;
+
+		
+	}
+}
+}
